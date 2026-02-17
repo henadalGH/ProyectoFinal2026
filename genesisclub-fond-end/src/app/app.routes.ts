2088f8ac853d471dbbfb04dad6271d4325df1670
henadalGH <hrnnnadal@gmail.com>
2026-02-17 08:08:06 -0300

    Subiendo correcciones

--- genesisclub-fond-end/src/app/app.routes.ts
+++ genesisclub-fond-end/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './ComponentesPublico/inicio/inicio';
 import { Login } from './ComponentesPublico/login/login';
-import { InicioAdmin } from './Administrador/inicio-admin/inicio-admin';
-import { HeaderAdmin } from './Administrador/header-admin/header-admin';
+
 
 export const routes: Routes = [
 
@@ -11,7 +10,4 @@ export const routes: Routes = [
     {path:"login", component: Login},
     {path:'', redirectTo: '/inicio', pathMatch: 'full'},
 
-    //Rutas del administrador
-    {path: "inicioAdmin", component: InicioAdmin},
-    {path: "headerAdmin", component: HeaderAdmin}
 ];
