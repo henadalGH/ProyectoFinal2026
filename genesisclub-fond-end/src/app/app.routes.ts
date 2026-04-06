54486cee330680dde31e474d6bafdaae24bfcb19
henadalGH <hrnnnadal@gmail.com>
2026-04-06 18:18:59 -0300

    Creacion de componente registro

--- genesisclub-fond-end/src/app/app.routes.ts
+++ genesisclub-fond-end/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './ComponentesPublico/inicio/inicio';
 import { Login } from './ComponentesPublico/login/login';
+import { RegistroCliente } from './ComponentesPublico/registro-cliente/registro-cliente';
 
 
 export const routes: Routes = [
@@ -9,5 +10,6 @@ export const routes: Routes = [
     {path:"inicio", component: Inicio},
     {path:"login", component: Login},
     {path:'', redirectTo: '/inicio', pathMatch: 'full'},
+    {path:'registroClietne', component: RegistroCliente}
 
 ];
